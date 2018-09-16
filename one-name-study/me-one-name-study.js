0729b05bd951d9890ce3b480f9c86b6e37b77e09
metherton <martinetherton63@gmail.com>
2018-09-16 22:29:18 +0200

    add one name study page

---
+++ one-name-study/me-one-name-study.js
@@ -0,0 +1,54 @@
+class MeOneNameStudy extends HTMLElement {
+
+  constructor() {
+    super();
+    // private variables
+    this._private1 = null;
+    //create a shadow root
+    this._root = this.attachShadow({"mode": "open"});
+  }
+
+  connectedCallback() {
+    this._root.innerHTML = `
+      
+      <p id="text">Etherton one name study</p>
+      
+    `;
+    this._$text = this._root.querySelector('#text'); //store important elements for later use..prefixing DOM elements with $
+    //  this._render();
+  }
+
+  _render() {
+    this._$text.innerText = '... is awesome !'; // selectively update only parts of the template which need to change
+  }
+
+  // observe attribute changes
+  static get observedAttributes() {
+    return ['an-important-attribute'];
+  }
+
+  // react to attribute changes
+  attributeChangedCallback(name, oldValue, newValue) {
+    // do stuff
+  }
+
+  // use setters and getters to create an API for the component
+  set property1(data) {
+    if (this._private1 === data) return;
+    this._private1 = data;
+    //  this._render();
+  }
+
+  get property1() {
+    return this._private1;
+  }
+
+  disconnectedCallback() {
+    // do stuff
+  }
+
+
+}
+
+window.customElements.define('me-one-name-study', MeOneNameStudy);
+
