df49574e4d686b95881feb7b50acb00b0ca3de63
metherton <martinetherton63@gmail.com>
2018-09-17 21:40:56 +0200

    use button

--- me-drawer.js
+++ me-drawer.js
@@ -10,23 +10,69 @@ class MeDrawer extends HTMLElement {
 
   connectedCallback() {
     this._root.innerHTML = `
+      <link rel="stylesheet" href="cssreset.css" type="text/css" />
       <style>
-        html, body, div {
-          height: 100%;
+        html, body {
+          height: 100%;          
         }
       
         #container {
+          overflow: hidden;
           position: fixed;
-          left: -600px;
-          max-width: 600px;
-          transition: transform 0.5s ease-in;
+          max-width: 650px;
+          height: 100%;
+          left: -650px;
+          z-index: 1;
+          transition: transform 1s;
+          width:100%;
+          
+        }
+        .close-drawer-container {
+          width: 100%;
+          text-align: right;
+        }
+        .close-drawer {
+          margin-right: 1em;
+        }
+        
+        .close-drawer:hover {
+          cursor: pointer;
         }
         
         #container.open {
-          transform: translateX(600px);
+          transform: translateX(650px);
         }
+        .info {
+          margin-top: 1em;
+          margin-bottom: 1em;
+          margin-left: 1em;
+        }
+        li {
+          margin-top: 1.0em;
+        }
+        button {
+          display: inline-block;
+          border: none;
+          margin: 0;
+          text-decoration: none;
+          background: transparent;
+          color: #0a0;
+          font-family: monospace;
+          font-size: 1em;
+          font-weight: bold;
+          cursor: pointer;
+          text-align: center;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+         }
+          button:focus {
+            outline: none;
+          }
       </style>
       <div id="container">
+        <div class="close-drawer-container">
+          <div class="close-drawer"><button id="collapse" class="button">x</button></div>      
+        </div>
         <p id="text"></p>
         <section>$&nbsp;printenv</section>
         <ul class="info">
@@ -35,15 +81,18 @@ class MeDrawer extends HTMLElement {
           <li>address=Marknesse,&nbsp;Netherlands</li>
           <li>hobbies=football,&nbsp;cycling,&nbsp;skiing</li>
         </ul>
-        <section>$</section>  
-       </div>`;
+        <section>$</section>
+         
+       </div>
+      
+    `;
     this._$container = this._root.querySelector("#container");
     this._$text = this._root.querySelector('#text'); //store important elements for later use..prefixing DOM elements with $
     //  this._render();
   }
 
   _render() {
-    this._$text.innerText = ''; // selectively update only parts of the template which need to change
+    //   this._$text.innerText = '... is awesome  dfasdf asd!'; // selectively update only parts of the template which need to change
   }
 
   // observe attribute changes
